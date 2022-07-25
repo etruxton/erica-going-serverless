d2552e9d808c67887dba39feb9f9f944543783ca
etruxton <awesomeerica743@gmail.com>
2022-07-24 20:13:19 -0500

    adding jwt decoding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 import "./App.css"
+//import jwt_decode from "jwt-decode";
+
 function App() {
-  const [input, setInput] = useState([{ text: ""}]);
+
+  //var decoded=jwt_decode(token);
+  //console.log(decoded);
+
+  //console.log(parseJwt(token));
+  //console.log(encodeJwtPlease());
+
+  const [input, setInput] = useState([{ header: "", payload: "", vsig: "", token: ""}]);
+
+  
 
   // handle input change
   const handleInputChange = (e, index) => {
@@ -12,24 +23,59 @@ function App() {
   };
   const json = JSON.stringify(input);
   const obj = JSON.parse(json);
+  console.log(obj[0].token);
+  console.log(JSON.stringify(parseJwt(obj[0].token)).replace(/","/g,"\",\n\""))
+  var token = JSON.stringify(parseJwt(obj[0].token)).replace(/","/g,"\",\n\"");
+  //console.log(JSON.stringify(parseJwt(token)));
   return (
     <body>
       <h1 class="App">Erica Going Serverless</h1>
       {input.map((x, i) => {
         return (
-          <div class="App">
-            <input
-              name="text"
-              placeholder="Enter text"
-              value={x.text}
+          <div class="textInput">
+            <textarea
+              name="token"
+              placeholder="Please Enter JWT Token."
+              value={x.token}
               onChange={e => handleInputChange(e, i)}
+              cols="60" rows="10"
             />
-          </div>
+            </div>
         );
       })}
-      <h2 class="App">{obj[0].text}</h2>
+      <div class = "textOutput">
+        <h2>
+          Payload:
+          {'\n'}
+          {token}
+        </h2>
+      </div>
+      
     </body>
   );
 }
+
+function parseJwt (token) {
+  try {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  } catch (err) {
+    console.log(err.message);
+    return {error: "invalid JWT token"}; 
+  }
+  return JSON.parse(jsonPayload);
+};
+/*function encodeJwtPlease() {
+  const sign = require('jwt-encode');
+  const secret = 'super secret key';
+  const data = {
+    text: "encoded text"
+  };
+  const jwt = sign(data, secret);
+  return jwt;
+};*/
 
 export default App;
